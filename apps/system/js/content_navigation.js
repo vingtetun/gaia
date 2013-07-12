84b5a1e78508fdebfcb2a76f95b7083b19285156
Etienne Segonzac <etienne@segonzac.info>
2013-07-12 18:24:52 +0200

    Fixing the in-content navigation on website. Cleaning up the de-activation for packaged apps.

--- apps/system/js/content_navigation.js
+++ apps/system/js/content_navigation.js
@@ -2,23 +2,25 @@
   'use strict';
 
   window.addEventListener('historychange', function onHistoryChange(evt) {
-    if (window.location.toString().indexOf('app://') === -1)  {
-      var history = evt.detail.current;
+    var history = evt.detail.current;
 
-      history.oncangoback = updateControlsDisplay;
-      history.oncangoforward = updateControlsDisplay;
-      history.onlocationchange = function locationChange() {
-        Rocketbar.setLocation(this.location);
-        updateControlsDisplay.call(this);
-      };
-
-      updateControlsDisplay.call(history);
-
-      var backButton = history.wrapper.querySelector('button.back');
-      backButton.onclick = history.goBack.bind(history)
-      var forwardButton = history.wrapper.querySelector('button.forward');
-      forwardButton.onclick = history.goForward.bind(history);
+    if (history.isApp) {
+      return;
     }
+
+    history.oncangoback = updateControlsDisplay;
+    history.oncangoforward = updateControlsDisplay;
+    history.onlocationchange = function locationChange() {
+      Rocketbar.setLocation(this.location);
+      updateControlsDisplay.call(this);
+    };
+
+    updateControlsDisplay.call(history);
+
+    var backButton = history.wrapper.querySelector('button.back');
+    backButton.onclick = history.goBack.bind(history)
+    var forwardButton = history.wrapper.querySelector('button.forward');
+    forwardButton.onclick = history.goForward.bind(history);
   });
 
   function updateControlsDisplay() {

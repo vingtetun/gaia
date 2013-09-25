a0755404ca832263d2826202fbd5adc1bd53378f
Michal Budzynski <michal@virtualdesign.pl>
2013-09-25 15:49:30 +0200

    [haida] task switcher flickering fix

--- apps/system/js/rocketbar.js
+++ apps/system/js/rocketbar.js
@@ -99,14 +99,15 @@ var Rocketbar = {
     this.results.innerHTML = '';
     this.input.value = '';
     this.bar.classList.add('open');
-
+    CardsView.showCardSwitcher();
+    
     //this.showRunningApps();
     
-    if (focus) {
+    //if (focus) {
       //this.input.focus();
       //this.results.classList.add('open');
-      CardsView.showCardSwitcher();
-    }
+      
+    //}
     
 
   },
@@ -118,6 +119,7 @@ var Rocketbar = {
    * @param {function} callback Called after the transition
    */
   close: function rocketbar_close(evenIfFocused, callback) {
+    CardsView.hideCardSwitcher();
     this.progress.classList.remove('loading');
     document.getElementById('screen').classList.remove('cards-view');
     document.getElementById('cards-view').classList.remove('active');
